d35052fa5357e8ae30bbe383c6996388822a0dcd
Akash Chauhan <aakashrajput1012@gmail.com>
2018-08-07 16:08:57 +0530

    New Changes

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -68,7 +68,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-	<h1>Staging Deployement</h1>
+	<h1>Staging Deployements</h1>
 
 	<div id="body">
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
